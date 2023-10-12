68b1b4a40747bd70ad2b0b210aa65f33d734d641
Vipul Tanwar <62182123+vipul-tanwar@users.noreply.github.com>
2023-10-12 23:56:33 +0530

    New Changes

--- src/App.js
+++ src/App.js
@@ -289,14 +289,13 @@ function App() {
     }
   );
 
-  
   function testnetName(temp) {
     if (temp?.includes("-")) {
       const words = temp.replaceAll("-", " ").slice(0, -9).split(" ");
       const capitalizedWords = words.map(
         (word) => word.charAt(0).toUpperCase() + word.slice(1)
       );
-      return capitalizedWords.join(" ");
+      return capitalizedWords.join("-");
     } else {
       return temp || "";
     }
@@ -335,8 +334,23 @@ function App() {
             {BuildbearLogo()}
             <div>BuildBear</div>
           </div>
-          <div style={{ display: 'flex', alignItems: "center", gap: "10px"}} >
-            <div style={{ border : "1px solid white ", width: "20px", height: "20px", borderRadius: "100%", fontSize: "14px", fontWeight: "600" }} >i</div>
+          <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+            <div
+              style={{
+                border: "1px solid white ",
+                width: "20px",
+                height: "20px",
+                borderRadius: "100%",
+                fontSize: "14px",
+                fontWeight: "600",
+              }}
+              class="tooltip"
+            >
+              i
+              <span class="tooltiptext">
+                To reset the configuration and establish a new sandbox.
+              </span>
+            </div>
             <button
               className="ide-button"
               onClick={() => {
@@ -464,93 +478,212 @@ function App() {
 
           <div style={{ fontSize: "20px" }}>
             {nodeId && (
-              <div
-                style={{
-                  display: "flex",
-                  // justifyContent: "center",
-                  alignItems: "center",
-                  gap: "10px",
-                  marginBottom: "6px",
-                  fontSize: "18px" 
-                }}
-              >
-                Sandbox Id: {testnetName(nodeId)}{" "}
-                {live ? (
-                  <span className="live-node "></span>
-                ) : (
-                  <span className=" notlive-node"></span>
-                )}{" "}
-              </div>
-            )}
-          </div>
-          <div
-            style={{
-              fontSize: "20px",
-              display: "flex",
-              // justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            {blockNumber && (
-              <div style={{ marginBottom: "16px", fontSize: "18px" }}>
-                {" "}
-                Blocknumber : {blockNumber}
-              </div>
-            )}
-          </div>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "start",
-              gap: "10px",
-            }}
-          >
-            {nodeId ? (
-              <>
-                <button
-                  onClick={() => {
-                    setShowRpc(!showRpc);
+              <div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "left",
+                    // justifyContent: "left",
+                    alignItems: "center",
+                    gap: "10px",
+                    marginBottom: "16px",
+                    fontSize: "18px",
                   }}
-                  className="ide-button"
                 >
-                  View & Copy Rpc
-                </button>
-                {showRpc && (
+                  <div>Sandbox Id: {testnetName(nodeId)} </div>
                   <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      // justifyContent: "left",
+                      // backgroundColor: "green",
+                      // alignItems: "center",
+                      gap: "10px",
+                      marginBottom: "6px",
+                      fontSize: "16px",
+                      paddingTop: "2px",
+                    }}
+                  >
+                    <div style={{ marginTop: "5px" }}> Sandbox Status : </div>
+                    <div
+                      style={{
+                        // backgroundColor: "red",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        float: "left",
+                        gap: "10px",
+                        marginBottom: "6px",
+                        fontSize: "16px",
+                        border: `1px solid ${live ? "green" : "red"}`,
+                        padding: "5px 8px",
+                        borderRadius: "20px",
+                      }}
+                    >
+                      {live ? (
+                        <span className="live-node "></span>
+                      ) : (
+                        <span className=" notlive-node"></span>
+                      )}{" "}
+                      {live ? (
+                        "Live"
+                      ) : (
+                        <>
+                          Starting <div class="loader"></div>
+                        </>
+                      )}
+                    </div>
+                    {live ? "":<div
+                      style={{
+                        border: "1px solid white ",
+                        width: "20px",
+                        height: "20px",
+                        borderRadius: "100%",
+                        fontSize: "14px",
+                        fontWeight: "600",
+                        marginTop: "6px",
+                      }}
+                      class="tooltip"
+                    >
+                      i
+                      <span class="tooltiptext">
+                      Kindly wait as the sandbox is being created.                     </span>
+                    </div> }
+                  </div>
+                </div>
+                <div></div>
+              </div>
+            )}
+          </div>
+
+          {live && (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "start",
+                gap: "10px",
+              }}
+            >
+              {nodeId ? (
+                <>
+                  <button
+                    onClick={() => {
+                      setShowRpc(!showRpc);
+                      // navigator.clipboard.writeText(
+                      //   `rpc.dev.buildbear.io/${nodeId}`
+                      // );
+                    }}
+                    className="ide-button"
+                  >
+                    View & Copy Rpc
+                  </button>
+                  {showRpc && (
+                    <div
+                      style={{
+                        backgroundColor: "#35384c",
+                        width: "100%",
+                        alignItems: "center",
+                        display: "flex",
+                        padding: "8px 0px",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <div style={{ marginLeft: "14px", fontSize: "18px" }}>
+                        rpc.dev.buildbear.io/{nodeId}{" "}
+                      </div>{" "}
+                      <button
+                        className=""
+                        style={{
+                          backgroundColor: "#6a7098",
+                          marginRight: "10px",
+                          // backgroundColor: "#595c76",
+                          borderRadius: "5px",
+                          color: "white",
+                          padding: "4px",
+                          border: "none",
+                          cursor: "pointer",
+                        }}
+                        onClick={() => {
+                          // navigator.clipboard.writeText(
+                          //   `rpc.dev.buildbear.io/${nodeId}`
+                          // );
+                        }}
+                      >
+                        {}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          shape-rendering="geometricPrecision"
+                          text-rendering="geometricPrecision"
+                          image-rendering="optimizeQuality"
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                          viewBox="0 0 438 511.52"
+                          height={28}
+                        >
+                          <path
+                            fill-rule="nonzero"
+                            d="M141.44 0h172.68c4.71 0 8.91 2.27 11.54 5.77L434.11 123.1a14.37 14.37 0 0 1 3.81 9.75l.08 251.18c0 17.62-7.25 33.69-18.9 45.36l-.07.07c-11.67 11.64-27.73 18.87-45.33 18.87h-20.06c-.3 17.24-7.48 32.9-18.88 44.29-11.66 11.66-27.75 18.9-45.42 18.9H64.3c-17.67 0-33.76-7.24-45.41-18.9C7.24 480.98 0 464.9 0 447.22V135.87c0-17.68 7.23-33.78 18.88-45.42C30.52 78.8 46.62 71.57 64.3 71.57h12.84V64.3c0-17.68 7.23-33.78 18.88-45.42C107.66 7.23 123.76 0 141.44 0zm30.53 250.96c-7.97 0-14.43-6.47-14.43-14.44 0-7.96 6.46-14.43 14.43-14.43h171.2c7.97 0 14.44 6.47 14.44 14.43 0 7.97-6.47 14.44-14.44 14.44h-171.2zm0 76.86c-7.97 0-14.43-6.46-14.43-14.43 0-7.96 6.46-14.43 14.43-14.43h136.42c7.97 0 14.43 6.47 14.43 14.43 0 7.97-6.46 14.43-14.43 14.43H171.97zM322.31 44.44v49.03c.96 12.3 5.21 21.9 12.65 28.26 7.8 6.66 19.58 10.41 35.23 10.69l33.39-.04-81.27-87.94zm86.83 116.78-39.17-.06c-22.79-.35-40.77-6.5-53.72-17.57-13.48-11.54-21.1-27.86-22.66-48.03l-.14-2v-64.7H141.44c-9.73 0-18.61 4-25.03 10.41C110 45.69 106 54.57 106 64.3v319.73c0 9.74 4.01 18.61 10.42 25.02 6.42 6.42 15.29 10.42 25.02 10.42H373.7c9.75 0 18.62-3.98 25.01-10.38 6.45-6.44 10.43-15.3 10.43-25.06V161.22zm-84.38 287.11H141.44c-17.68 0-33.77-7.24-45.41-18.88-11.65-11.65-18.89-27.73-18.89-45.42v-283.6H64.3c-9.74 0-18.61 4-25.03 10.41-6.41 6.42-10.41 15.29-10.41 25.03v311.35c0 9.73 4.01 18.59 10.42 25.01 6.43 6.43 15.3 10.43 25.02 10.43h225.04c9.72 0 18.59-4 25.02-10.43 6.17-6.17 10.12-14.61 10.4-23.9z"
+                          />
+                        </svg>git push -u origin main
+                      </button>{" "}
+                    </div>
+                  )}
+
+                  {/* <div
                     style={{
                       backgroundColor: "#35384c",
                       fontSize: "14px",
                       padding: "10px 12px",
+                      // justifyContent: "space-between",
                       display: "flex",
                       gap: "16px",
                       alignItems: "center",
+                      marginRight: "20px",
                     }}
                   >
                     <div>rpc.dev.buildbear.io/{nodeId} </div>{" "}
-                    <button className="ide-button">Copy</button>{" "}
-                  </div>
-                )}
+                    <button className="" style={{ backgroundColor: "#6a7098" }}>
+                      {}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        shape-rendering="geometricPrecision"
+                        text-rendering="geometricPrecision"
+                        image-rendering="optimizeQuality"
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        viewBox="0 0 438 511.52"
+                        height={30}
+                      >
+                        <path
+                          fill-rule="nonzero"
+                          d="M141.44 0h172.68c4.71 0 8.91 2.27 11.54 5.77L434.11 123.1a14.37 14.37 0 0 1 3.81 9.75l.08 251.18c0 17.62-7.25 33.69-18.9 45.36l-.07.07c-11.67 11.64-27.73 18.87-45.33 18.87h-20.06c-.3 17.24-7.48 32.9-18.88 44.29-11.66 11.66-27.75 18.9-45.42 18.9H64.3c-17.67 0-33.76-7.24-45.41-18.9C7.24 480.98 0 464.9 0 447.22V135.87c0-17.68 7.23-33.78 18.88-45.42C30.52 78.8 46.62 71.57 64.3 71.57h12.84V64.3c0-17.68 7.23-33.78 18.88-45.42C107.66 7.23 123.76 0 141.44 0zm30.53 250.96c-7.97 0-14.43-6.47-14.43-14.44 0-7.96 6.46-14.43 14.43-14.43h171.2c7.97 0 14.44 6.47 14.44 14.43 0 7.97-6.47 14.44-14.44 14.44h-171.2zm0 76.86c-7.97 0-14.43-6.46-14.43-14.43 0-7.96 6.46-14.43 14.43-14.43h136.42c7.97 0 14.43 6.47 14.43 14.43 0 7.97-6.46 14.43-14.43 14.43H171.97zM322.31 44.44v49.03c.96 12.3 5.21 21.9 12.65 28.26 7.8 6.66 19.58 10.41 35.23 10.69l33.39-.04-81.27-87.94zm86.83 116.78-39.17-.06c-22.79-.35-40.77-6.5-53.72-17.57-13.48-11.54-21.1-27.86-22.66-48.03l-.14-2v-64.7H141.44c-9.73 0-18.61 4-25.03 10.41C110 45.69 106 54.57 106 64.3v319.73c0 9.74 4.01 18.61 10.42 25.02 6.42 6.42 15.29 10.42 25.02 10.42H373.7c9.75 0 18.62-3.98 25.01-10.38 6.45-6.44 10.43-15.3 10.43-25.06V161.22zm-84.38 287.11H141.44c-17.68 0-33.77-7.24-45.41-18.88-11.65-11.65-18.89-27.73-18.89-45.42v-283.6H64.3c-9.74 0-18.61 4-25.03 10.41-6.41 6.42-10.41 15.29-10.41 25.03v311.35c0 9.73 4.01 18.59 10.42 25.01 6.43 6.43 15.3 10.43 25.02 10.43h225.04c9.72 0 18.59-4 25.02-10.43 6.17-6.17 10.12-14.61 10.4-23.9z"
+                        />
+                      </svg>
+                    </button>{" "}
+                  </div> */}
 
-                <a
-                  href={`https://explorer.dev.buildbear.io/${nodeId}`}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="ide-button">Go to Explorer</button>
-                </a>
-                <a
-                  href={`https://faucet.dev.buildbear.io/${nodeId}`}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="ide-button">Got to Faucet</button>
-                </a>
-              </>
-            ) : (
-              ""
-            )}
-          </div>
+                  <a
+                    href={`https://explorer.dev.buildbear.io/${nodeId}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <button className="ide-button">Go to Explorer</button>
+                  </a>
+                  <a
+                    href={`https://faucet.dev.buildbear.io/${nodeId}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <button className="ide-button">Go to Faucet</button>
+                  </a>
+                </>
+              ) : (
+                ""
+              )}
+            </div>
+          )}
           {/* <button
             onClick={() => {
               getNodeDetails();
